35896f090fad1ee8d18a5d00e1d9877c5929ca5a
TabberAnimates <94705401+TabberAnimates@users.noreply.github.com>
2022-08-31 03:28:33 -0400

    Update main.js

--- edgenuity_addons/main.js
+++ edgenuity_addons/main.js
@@ -17,17 +17,13 @@ function createIframeElement(contents) {
 
 const order = 'afterbegin';
 
-// Auto-Complete (WIP)
-
-createIframeElement('<script>completeClass = " FrameComplete";</script>');
-
 // Button 1
 
 createIframeElement(`<button onclick="let person = prompt('Please enter a number to skip to', '1'); if ((person - 0) < 1) { person = 1; } API.FrameChain.openFrame(person);">Skip to Part</button>`);
 
 // Button 2
 
-createIframeElement('<button onclick="let array = API.FrameChain.framesStatus; let result = array.splice(array.indexOf('incomplete'), 1, 'complete'); API.FrameChain.framesStatus = array; API.FrameChain.openFrame(API.FrameChain.currentFrame + 1);">Skip Video</button>');
+createIframeElement(`<button onclick="let array = API.FrameChain.framesStatus; let result = array.splice(array.indexOf('incomplete'), 1, 'complete'); API.FrameChain.framesStatus = array; API.FrameChain.openFrame(API.FrameChain.currentFrame + 1);">Skip Video</button>`);
 
 // Button 3
 
