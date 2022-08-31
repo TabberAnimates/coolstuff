ac26f16efe5c3ff8cea9a8283a7070d1d922978e
TabberAnimates <94705401+TabberAnimates@users.noreply.github.com>
2022-08-30 22:41:27 -0400

    Update main.js

--- edgenuity_addons/main.js
+++ edgenuity_addons/main.js
@@ -1,3 +1,6 @@
+const div2 = document.createElement('div'); 
+div2.innerHTML = `<button onclick='let person = prompt("Please enter a number to skip to", "1"); if ((person - 0) < 1) { person = 1; } API.FrameChain.openFrame(person);'>Skip to Part</button>`; 
+document.body.insertAdjacentElement('afterbegin', div2);
 const div = document.createElement('div'); 
 div.innerHTML = `<button onclick="API.FrameChain.openFrame(API.FrameChain.currentFrame + 1)">Skip Video</button>`; 
 document.body.insertAdjacentElement('afterbegin', div);
