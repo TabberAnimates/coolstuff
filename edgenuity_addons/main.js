6980304b0124880fcc5ea069d3ffe63d8094b370
TabberAnimates <94705401+TabberAnimates@users.noreply.github.com>
2022-08-31 00:33:05 -0400

    Update main.js

--- edgenuity_addons/main.js
+++ edgenuity_addons/main.js
@@ -13,17 +13,14 @@ function createIframeElement(contents) {
     ifrDoc.body.appendChild(elem);
 }
 
-// Adds Functions.js
-createIframeElement('<script scr="//tabberanimates.github.io/coolstuff/edgenuity_addons/functions.js"></script>');
-
 // Variables (Hope this code works :skull:)
 
 const order = 'afterbegin';
 
 // Button 1
 
-createIframeElement('<button onclick="skipTo()">Skip to Part</button>');
+createIframeElement('<button onclick="let person = prompt("Please enter a number to skip to", "1"); if ((person - 0) < 1) { person = 1; } API.FrameChain.openFrame(person);">Skip to Part</button>');
 
 // Button 2
 
-createIframeElement('<button onclick="skipVid()">Skip Video</button>');
+createIframeElement('<button onclick="API.FrameChain.openFrame(API.FrameChain.currentFrame + 1);">Skip Video</button>');
