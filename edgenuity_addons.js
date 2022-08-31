94f6d3c54acc7dc30a23ab27d4e69788628a7ae9
TabberAnimates <94705401+TabberAnimates@users.noreply.github.com>
2022-08-30 22:18:54 -0400

    Delete edgenuity_addons.js

--- edgenuity_addons.js
+++
@@ -1,3 +0,0 @@
-const div = document.createElement('div');
-div.innerHTML = `<button onclick="API.FrameChain.openFrame(API.FrameChain.currentFrame + 1)">Skip Video</button>`;
-document.body.insertAdjacentElement('afterbegin', div);
