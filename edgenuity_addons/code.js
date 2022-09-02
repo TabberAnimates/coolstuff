b85b73a1a37c35163b413562190477c38a6e891e
TabberAnimates <94705401+TabberAnimates@users.noreply.github.com>
2022-09-02 19:27:09 -0400

    Update code.js

--- edgenuity_addons/code.js
+++ edgenuity_addons/code.js
@@ -13,19 +13,11 @@ function createIframeElement(contents) {
     ifrDoc.body.appendChild(elem);
 }
 
-// Variables (Hope this code works :skull:)
-
-const order = 'afterbegin';
-
 // Button 1
 
-createIframeElement(`<button onclick="let person = prompt('Please enter a number to skip to', '1'); if ((person - 0) < 1) { person = 1; } let array = API.FrameChain.framesStatus; let result = array.splice(array.indexOf('incomplete'), 1, 'complete'); let results = array.splice(person, 1, 'complete'); API.FrameChain.frameStatues = array; API.FrameChain.openFrame(person);">Skip to Part</button>`);
+createIframeElement(`<button onclick="let array = API.FrameChain.framesStatus; let result = array.splice(array.indexOf('incomplete'), 1, 'complete'); API.FrameChain.framesStatus = array; if (API.FrameChain.currentFrame != API.FrameChain.frameStatus.length) { API.FrameChain.openFrame(API.FrameChain.currentFrame + 1); } else { alert('The ability to go to the next activity has been unlocked.'); }">Skip Video</button>`);
 
 // Button 2
-
-createIframeElement(`<button onclick="let array = API.FrameChain.framesStatus; let result = array.splice(array.indexOf('incomplete'), 1, 'complete'); API.FrameChain.framesStatus = array; API.FrameChain.openFrame(API.FrameChain.currentFrame + 1);">Skip Video</button>`);
-
-// Button 3
 
 createIframeElement(`<button onclick="let array = API.FrameChain.framesStatus; for (var i = 0; i < array.length; i++) { if (array[i] == 'incomplete') { array[i] = 'complete'; } } API.FrameChain.frameStatus = array;">Auto Completion (In Development)</button>`);
 
